0bc0d113da5da7639bdf07fa62f98b8946d9d688
Acer <paingpyaethu@gmail.com>
2022-03-28 15:25:11 +0630

    first commit

---
+++ spa-vue/resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Vue - SPA</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    
+<h1 class="text-center mt-3">Laravel + Vue (SPA)</h1>
+
+<div id="app">
+    <example-component></example-component>
+</div>
+
+
+
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
